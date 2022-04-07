541a692d6e08cacc27e37c7decd81815c10656e5
narny29 <nayanjyotikakati1997@gmail.com>
2022-04-07 18:24:41 +0530

    Add files via upload

--- bitgo/constants/__init__.py
+++ bitgo/constants/__init__.py
@@ -3,4 +3,4 @@ BLOCKSTREAM_BLOCK_INFO_URL = "https://blockstream.info/api/block/{block_id}"
 BLOCKSTREAM_TRANSACTIONS_FOR_BLOCK_URL = "https://blockstream.info/api/block/{block_id}/txs/{transaction_init_count}"
 
 
-BLOCK_TRANSACTIONS_FILE_NAME = "constants/blocks/{block_id}.json"
+BLOCK_TRANSACTIONS_FILE_NAME = "constants/{block_id}.json"
